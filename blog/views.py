b79f772ad74730a1a4f9d5d230c262849d6b90b9
Ansesteri <angrylemon5678@gmail.com>
2023-12-07 20:04:02 +0200

    Add a lot of comment stuff

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,10 @@ from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
-from .forms import EmailPostForm
+from .forms import CommentForm, EmailPostForm
 from .models import Post
 
 class PostListView(ListView):
@@ -29,9 +30,15 @@ def post_list(request):
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, status=Post.Status.PUBLISHED)
-    
-    return render(request, 'blog/post/detail.html', {'post': post})
+    post = get_object_or_404(Post, slug=post, status=Post.Status.PUBLISHED,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    # Список активних коментарів до цього посту
+    comments = post.comments.filter(active=True)
+    # Форма для коментування користувачами
+    form = CommentForm()
+    return render(request, "blog/post/detail.html", {"post": post, 'comments': comments, 'form': form})
 
 def post_share(request, post_id):
     # Отримати пост за id
@@ -53,3 +60,22 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(
+        request,
+        "blog/post/comment.html",
+        context={
+            "post": post,
+            "form": form,
+            "comment": comment,
+        }
+    )
--- core/settings.py
+++ core/settings.py
@@ -31,14 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'blog',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "blog",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
